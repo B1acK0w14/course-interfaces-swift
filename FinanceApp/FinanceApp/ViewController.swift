cba0272e84329554851aabd72813771e6d372ddb
David Penagos <davisto83pena@gmail.com>
2019-06-06 00:51:47 -0500

    Add structure of the proyect

---
+++ FinanceApp/FinanceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by David Penagos on 6/6/19.
+//  Copyright © 2019 BlacKow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
